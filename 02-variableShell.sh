b4979f35ea3deac15506a5d714034aeb60465726
yesdani <yesdaniece@gmail.com>
2023-08-21 12:53:59 +0100

    making changes to 1,3 and 4 files

--- 02-variableShell.sh
+++ 02-variableShell.sh
@@ -3,4 +3,4 @@ person2=$2
 echo "$person1: how are you $person2"
 echo "$person2 : I am goo, how are you $person1"
 echo "$person1: good morning $person2"
-echo "$person2: Good Morning $person2"
+echo "$person2: Good Morning $person1"
