e2d804bdc2842cc33d369ad99ceadbd54b24922c
yesdani <yesdaniece@gmail.com>
2023-08-21 12:19:15 +0100

    variables commit 2

--- 02-variableShell.sh
+++ 02-variableShell.sh
@@ -1,4 +1,6 @@
-echo "Ramesh: how are you Suresh"
-echo "Suresh : I am goo, how are you Ramesh"
-echo "Ramesh: good morning Suresh"
-echo "Suresh: Good Morning Ramesh"
+person1=Ramesh
+person2=Suresh
+echo "$person1: how are you $person2"
+echo "$person2 : I am goo, how are you $person1"
+echo "$person1: good morning $person2"
+echo "$person2: Good Morning $person2"
