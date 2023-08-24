240454642986290d1c5ec08d749a19b3865b770d
yesdani <yesdaniece@gmail.com>
2023-08-24 17:42:37 +0100

    mysql intallation

--- mysqlinstall.sh
+++ mysqlinstall.sh
@@ -2,8 +2,8 @@
 
 USERID=$(id -u)
 
-if [$USERID -ne 0 ];
-then
- echo " ERROR: please run the script with root access"
+if [ $USERID -ne 0 ];
+  then
+   echo " ERROR: please run the script with root access"
 
- fi
+fi
