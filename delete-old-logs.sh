4e772839b0cfa0ea9ccf02de771d4fbc39218096
yesdani <yesdaniece@gmail.com>
2023-09-02 18:46:28 +0100

    commiting

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -9,3 +9,10 @@ LOGFILE=/home/centos/$SCRIPT_NAME-$DATE.log
 
 FILES_TO_DELETE=$(find  $APP_LOGS -name "*.log" -type f -mtime +15)
 echo "$FILES_TO_DELETE"
+
+while read line
+
+do 
+  echo "Deleting $line" &>>$LOGFILE
+  rm -rf $line
+done < $FILES_TO_DELETE
