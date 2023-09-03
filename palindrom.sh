b4ff29ef87bb96f612128868b0ea5aea7523bdce
yesdani <yesdaniece@gmail.com>
2023-09-03 23:22:56 +0100

    commiting

---
+++ palindrom.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Enter a word to check palindrom:"
+read WORD
+
+echo "Entered word is: $WORD"
+
+REVWORD=$( echo "$WORD" | rev )
+
+if ($WORD !ne $REVWORD)
+then
+
+  echo " $WORD is not palindrom"
+else
+   echo "$WORD is palindrom"
+fi
