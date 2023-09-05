56244c8b64be4eba589687607db60ba09d03be5f
yesdani <yesdaniece@gmail.com>
2023-09-05 22:11:31 +0100

    commiting

--- DiskUsage.sh
+++ DiskUsage.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-SCRIPT_NAME=$0
-LOGFILE_DIRECTORY=/tmp
-LOGFILE=$LOGFILE_DIRECTORY/$SCRIPT_NAME-$DATE.log
-DATE=$(date +%F:%H:%M:%S)
+#SCRIPT_NAME=$0
+#LOGFILE_DIRECTORY=/tmp
+#LOGFILE=$LOGFILE_DIRECTORY/$SCRIPT_NAME-$DATE.log
+#DATE=$(date +%F:%H:%M:%S)
 
 # R="\e[31m"
 # G="\e[32m"
@@ -11,13 +11,12 @@ DATE=$(date +%F:%H:%M:%S)
 
  DISK_USAGE=$(df -hT  | grep -vE 'tmpfs|Filesystem' )
 
- DISK_USAGE_THRESHOLD=1
+ # DISK_USAGE_THRESHOLD=1
 #IFS= Means internal field seperator is space
 
  while IFS= read line
  do
    echo "output: $line" 
-   echo $DISK_USAGE_THRESHOLD
-   echo $LOGFILE
+
  done  <<< $DISK_USAGE
 
