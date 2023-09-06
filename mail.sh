840b9fa0ee7b1cdb986e33e8baeac283f8f7c6c3
yesdani <yesdaniece@gmail.com>
2023-09-06 11:17:52 +0100

    commiting

--- mail.sh
+++ mail.sh
@@ -6,6 +6,6 @@
  ALERT_TYPE=$5
 
 echo "$BODY"
- FINAL_BODY=$(sed -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/message/$BODY/g" template.html)
+ FINAL_BODY=$(sed -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/Message/$BODY/g" template.html)
  echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" "$TO_ADDRESS"
  #echo " $TO_ADDRESS $SUBJECT $BODY $TEAM_NAME $ALERT_TYPE" 
