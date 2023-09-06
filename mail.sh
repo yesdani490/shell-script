0d981c7204465b5415f27e4af4c2d48f8c3ba91b
yesdani <yesdaniece@gmail.com>
2023-09-06 11:45:19 +0100

    commiting

--- mail.sh
+++ mail.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
  TO_ADDRESS=$1
  SUBJECT=$2
- #BODY=$(sed -e 's/[]\/$*.^[]/\\&/g' <<< $3)
- #BODY=$(echo "$3" | sed 's/[][\/$*.^[]/\\&/g')
-
  BODY=$3  # Assign the entire message to BODY
+  TEAM_NAME=$4
+ ALERT_TYPE=$5
 
 # Use BODY in the sed expression, escaping any special characters
 BODY=$(echo $BODY| sed 's/[][\/$*.^[]/\\&/g')
 
 
- TEAM_NAME=$4
- ALERT_TYPE=$5
+
 
 #echo "$BODY"
  FINAL_BODY=$(sed -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/Message/$BODY/g" template.html)
