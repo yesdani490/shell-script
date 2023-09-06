37d7c7c976ec4bebb8101317dd5401e436bef5c3
yesdani <yesdaniece@gmail.com>
2023-09-06 10:10:01 +0100

    commiting

--- mail.sh
+++ mail.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
  TO_ADDRESS=$1
  SUBJECT=$2
- BODY=$3
+ BODY=$(sed -e 's/[]\/$*.^[]/\\&/g' <<< $3)
+ 
+ 
  TEAM_NAME=$4
  ALERT_TYPE=$5
 
  #echo " ALL args: $*@"
- echo "$BODY" | mail -s "$SUBJECT" "$TO_ADDRESS"
+ FINAL_BODY=$(sed -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/MESSAGE/$BODY" template.html)
+ echo "$FINAL_BODY" | mail -s "$SUBJECT" "$TO_ADDRESS"
  echo " $TO_ADDRESS $SUBJECT $BODY $TEAM_NAME $ALERT_TYPE"
