e256e7afc0a6267a9116e1098868aede98aa018e
yesdani <yesdaniece@gmail.com>
2023-09-10 08:04:29 +0100

    checking user exits or not

---
+++ user.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Please enter username"
+read USERNAME
+
+# Use the 'id' command to check if the user exists
+id "$USERNAME" 
+
+# Check the exit status of the 'id' command
+if [ $? -eq 0 ]; then
+  echo "User '$USERNAME' exists."
+else
+  echo "User '$USERNAME' does not exist ."
+  useradd $USERNAME
+fi
+
+echo "username  is:"$USERNAME
